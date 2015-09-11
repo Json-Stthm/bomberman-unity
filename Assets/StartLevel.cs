a22f3c273d4a191b69da9efb340125ab2df3fc5c
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-09-12 00:14:23 +0200

    Menu update

---
+++ Assets/StartLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartLevel : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadLevel () {
+		Application.LoadLevel (1);
+	}
+}
