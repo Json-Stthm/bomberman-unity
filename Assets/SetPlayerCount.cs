60ae56b732360b26f5581e857c4fac2b103ddc35
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-09-11 23:44:42 +0200

    Menu

---
+++ Assets/SetPlayerCount.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SetPlayerCount : MonoBehaviour {
+
+	public void ChangeValue(float value) {
+
+		GetComponent<Text> ().text = value.ToString () + " PLAYERS";
+	}
+
+}
